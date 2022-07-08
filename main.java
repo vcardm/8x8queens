292c1f484b1cbe571ca67f0683ff11a8360a840c
vcardm <87623211+vcardm@users.noreply.github.com>
2022-07-08 09:33:46 -0400

    create main

---
+++ main.java
@@ -0,0 +1,15 @@
+//imports
+import java.util.*;
+
+//create main method
+public class Queens {
+  private int [][] map = new int[8][8];
+  private int [][] testMap = new int[8][8];
+  private int heuristic = 0;
+  private int queenLocs = 0;
+  private int restarts = 0;
+  private int moves = 0;
+  private boolean newMap = true;
+  private int neighbors = 8;
+  
+}
