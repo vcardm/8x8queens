a5bffdc34ddf89166d199cae7f85bd6faa1ebe14
vcardm <87623211+vcardm@users.noreply.github.com>
2022-07-10 15:19:22 -0400

    Update main.java

--- main.java
+++ main.java
@@ -1,15 +1,8 @@
 //imports
 import java.util.*;
 
-//create main method
+//
 public class Queens {
   private int [][] map = new int[8][8];
-  private int [][] testMap = new int[8][8];
-  private int heuristic = 0;
-  private int queenLocs = 0;
-  private int restarts = 0;
-  private int moves = 0;
-  private boolean newMap = true;
-  private int neighbors = 8;
-  
+  private int queenSpot = 0;
 }
