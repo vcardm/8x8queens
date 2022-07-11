3bd42ddff44208c687734ac234f2b3281d1f80c1
vcardm <87623211+vcardm@users.noreply.github.com>
2022-07-11 12:37:36 -0400

    Update main.java

--- main.java
+++ main.java
@@ -1,6 +1,7 @@
+//main method to run the program
 public static void main(String[] args) {
-  Queens test = new Queens( );
-  test.randomiMap();
+  Queens test = new Queens();
+  test.randomMap();
   test.moveQueen();
-`}
+  }
 }
